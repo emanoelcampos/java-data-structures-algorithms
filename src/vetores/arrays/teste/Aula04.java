7e942f57b3ab237ab8e68090f1879a84fcd95eae
emanoelcampos <emanoel.cv@gmail.com>
2023-07-12 12:50:34 -0300

    testando o método toString da classe Vetor

---
+++ src/vetores/arrays/teste/Aula04.java
@@ -0,0 +1,18 @@
+package vetores.arrays.teste;
+
+import vetores.arrays.vetor.Vetor;
+
+public class Aula04 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor =  new Vetor(5);
+
+        vetor.adiciona("emanoel");
+        vetor.adiciona("campos");
+
+        System.out.println(vetor.getTamanho());
+        System.out.println(vetor);
+
+    }
+}
