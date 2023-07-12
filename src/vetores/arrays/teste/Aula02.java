57428a18bab70bf3aa6b752e45d006ac2c44f993
emanoelcampos <emanoel.cv@gmail.com>
2023-07-12 11:22:23 -0300

    classe teste para classe vetor

---
+++ src/vetores/arrays/teste/Aula02.java
@@ -0,0 +1,12 @@
+package vetores.arrays.teste;
+
+import vetores.arrays.vetor.Vetor;
+
+public class Aula02 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(3);
+
+    }
+}
