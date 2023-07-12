a26f28b091b8b67e1d99be34268da7da280d1eac
emanoelcampos <emanoel.cv@gmail.com>
2023-07-12 12:06:16 -0300

    teste do método adiciona da classe vetor

---
+++ src/vetores/arrays/teste/Aula03.java
@@ -0,0 +1,16 @@
+package vetores.arrays.teste;
+
+import vetores.arrays.vetor.Vetor;
+
+public class Aula03 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("emanoel");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("campos");
+
+    }
+}
