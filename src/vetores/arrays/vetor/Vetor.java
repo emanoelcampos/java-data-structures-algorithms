d8b49bb25ad6111ba70d0ed22aa520043ea2d54f
emanoelcampos <emanoel.cv@gmail.com>
2023-07-12 11:21:56 -0300

    classe vetor com atributo array de elementos do tipo String e construtor para inicializar o array de String com capacidade

---
+++ src/vetores/arrays/vetor/Vetor.java
@@ -0,0 +1,10 @@
+package vetores.arrays.vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
