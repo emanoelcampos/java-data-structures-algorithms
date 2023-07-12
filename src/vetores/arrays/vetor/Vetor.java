aa6644b9d8d4c6b27d4b7be6ac2f8f08be5c8c78
emanoelcampos <emanoel.cv@gmail.com>
2023-07-12 12:05:36 -0300

    método adiciona que verifica se índice do último elemento é menor que o tamanho da capacidade do array e adiciona um novo elemento no final

--- src/vetores/arrays/vetor/Vetor.java
+++ src/vetores/arrays/vetor/Vetor.java
@@ -3,8 +3,38 @@ package vetores.arrays.vetor;
 public class Vetor {
 
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade){
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    /*public void adiciona(String elemento) {
+        for(int i = 0; i < this.elementos.length; i++){
+            if(elementos[i] == null) {
+                elementos[i] = elemento;
+                break;
+            }
+        }
+    }*/
+
+    /*public void adiciona(String elemento) throws Exception {
+
+        if(this.tamanho < this.elementos.length){
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        } else {
+            throw new Exception("Vetor jáestá cheio, não é possível adicionar mais elementos");
+        }
+    }*/
+
+    public boolean adiciona(String elemento) {
+        if(this.tamanho < this.elementos.length){
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
     }
 }
