7229f0930419a25f3c91299c3d3986efb1e493b2
emanoelcampos <emanoel.cv@gmail.com>
2023-07-13 10:20:17 -0300

    teste método de busca de elemento

---
+++ src/vetores/arrays/teste/Aula06.java
@@ -0,0 +1,16 @@
+package vetores.arrays.teste;
+
+import vetores.arrays.vetor.Vetor;
+
+public class Aula06 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor =  new Vetor(5);
+
+        vetor.adiciona("emanoel");
+        vetor.adiciona("campos");
+
+        System.out.println(vetor.busca("campos"));
+    }
+}
