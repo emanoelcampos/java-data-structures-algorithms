03047731936c4f226c57d10bbafbd1a3c54cf6df
emanoelcampos <emanoel.cv@gmail.com>
2023-07-13 13:01:45 -0300

    teste do método aumentaCapacidade que aumenta a capacidade do array conforme necessidade

---
+++ src/vetores/arrays/teste/Aula08.java
@@ -0,0 +1,17 @@
+package vetores.arrays.teste;
+
+import vetores.arrays.vetor.Vetor;
+
+public class Aula08 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("emanoel");
+        vetor.adiciona("campos");
+        vetor.adiciona("viana");
+
+        System.out.println(vetor);
+    }
+}
