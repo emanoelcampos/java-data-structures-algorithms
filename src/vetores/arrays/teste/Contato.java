bf7aa7cc088a91456cdf52c8bdc5000dfbdb0013
emanoelcampos <emanoel.cv@gmail.com>
2023-07-13 16:22:14 -0300

    classe para implementar testes da classe generica de vetor

---
+++ src/vetores/arrays/teste/Contato.java
@@ -0,0 +1,35 @@
+package vetores.arrays.teste;
+
+public class Contato {
+
+    private String nome;
+    private String telefone;
+    private String email;
+
+    public Contato(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+                "nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
