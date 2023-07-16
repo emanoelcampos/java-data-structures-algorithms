7eeab13b433f045e36836d78690382a0e9b9f578
emanoelcampos <emanoel.cv@gmail.com>
2023-07-16 09:28:23 -0300

    criação de classe Lista2 que extende a classe genérica para reaproveitamento de código

---
+++ src/estrutura/dados/vetores/arrays/vetor/Lista2.java
@@ -0,0 +1,22 @@
+package estrutura.dados.vetores.arrays.vetor;
+
+import estrutura.dados.base.EstruturaEstatica;
+
+public class Lista2<T> extends EstruturaEstatica<T> {
+
+    public Lista2() {
+        super();
+    }
+
+    public Lista2(int capacidade) {
+        super(capacidade);
+    }
+
+    public boolean adiciona(T elemento) {
+        return super.adiciona(elemento);
+    }
+
+    public boolean adiciona(int posicao, T elemento) {
+        return super.adiciona(posicao, elemento);
+    }
+}
