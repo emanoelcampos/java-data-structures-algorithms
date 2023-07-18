3348188b19aa1a824eb4df58cf4dabcbf6a5d70f
emanoelcampos <emanoel.cv@gmail.com>
2023-07-18 13:25:55 -0300

    criação da classe Fila extendendo a classe EstruturaEstatica e criando construtor

---
+++ src/estrutura/dados/filas/fila/Fila.java
@@ -0,0 +1,15 @@
+package estrutura.dados.filas.fila;
+
+import estrutura.dados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+}
