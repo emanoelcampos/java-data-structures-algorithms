a898ca41d6c6ed015c50921bc5f4c88f07d109c8
emanoelcampos <emanoel.cv@gmail.com>
2023-07-18 13:24:58 -0300

    testando a construção da classe Fila

---
+++ src/estrutura/dados/filas/teste/Aula19.java
@@ -0,0 +1,14 @@
+package estrutura.dados.filas.teste;
+
+import estrutura.dados.filas.fila.Fila;
+
+public class Aula19 {
+
+    public static void main(String[] args) {
+
+        Fila<String> fila = new Fila<>();
+
+        System.out.println(fila.estaVazio());
+        System.out.println(fila.getTamanho());
+    }
+}
