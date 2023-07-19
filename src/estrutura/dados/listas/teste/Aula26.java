3635bdb456575ecddcb4b545bf2c6d4e6bb4539d
emanoelcampos <emanoel.cv@gmail.com>
2023-07-18 21:01:38 -0300

    teste de implementação do método de adicionar elementos da classe ListaEncadeada

---
+++ src/estrutura/dados/listas/teste/Aula26.java
@@ -0,0 +1,14 @@
+package estrutura.dados.listas.teste;
+
+import estrutura.dados.listas.lista.ListaEncadeada;
+
+public class Aula26 {
+
+    public static void main(String[] args) {
+
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+
+        System.out.println(lista);
+    }
+}
